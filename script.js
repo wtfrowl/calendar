87e796bdc36f6ea9d58b75c781a1593155f236c0
Rahul <116852257+wtfrowl@users.noreply.github.com>
2023-11-06 00:46:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+            const date = document.getElementById("date");
+			const day = document.getElementById("day");
+			const month = document.getElementById("month");
+			const year = document.getElementById("year");
+			const time = document.getElementById("time");
+            const name = document.getElementById("name");
+			const calendarInIndianLanguages = [
+				"पंचांग",   // Hindi
+				"காலண்டர்", // Tamil
+				"క్యాలెండర్",  // Telugu
+				"পঞ্জিকা",    // Bengali
+				"પંચાંગ",    // Gujarati
+				"କ୍ୟାଲେଣ୍ଡର୍",  // Odia
+				"ਕੈਲੰਡਰ",   // Punjabi
+				"കലണ്ടർ",     // Malayalam
+				"ಕ್ಯಾಲೆಂಡರ್",   // Kannada
+				"कॅलेंडर",   // Marathi
+				"سالنامه",     // Urdu
+				"বার্ষিক পত্রিকা", // Assamese
+				"ସାଲଣ୍ଡର",   // Sambalpuri
+				"क्यालेंडर",  // Nepali
+				"পঞ্জিকা",    // Maithili
+				"நாள்காட்டி",   // Malayalam (Transliteration)
+				"ਕੈਲੰਡਰ",   // Punjabi (Gurmukhi)
+				"પંચાંગ",    // Gujarati (Transliteration)
+				"পঞ্জিকা",    // Bodo
+				"कॅलेंडर",   // Konkani
+				"காலண்டர்", // Tamil (Transliteration)
+				"క్యాలెండర్",  // Telugu (Transliteration)
+				"കലണ്ടർ",     // Malayalam (Transliteration)
+				"ಕ್ಯಾಲೆಂಡರ್",   // Kannada (Transliteration)
+				"कॅलेंडर",   // Konkani (Transliteration)
+				"ਕੈਲੰਡਰ",   // Dogri
+				"کیلنڈر",    // Kashmiri
+				"କ୍ୟାଲେଣ୍ଡର",  // Odia (Transliteration)
+				"संवत सरणी",   // Sanskrit
+				"நாளாட்டு",   // Tamil (Transliteration)
+				"పంచాంగం",   // Telugu (Transliteration)
+				"പഞ്ചാംഗം",   // Malayalam (Transliteration)
+				"ಪಂಚಾಂಗ",    // Kannada (Transliteration)
+			  ];
+			  
+			const days=["SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY","FRIDAY"]
+			const months = [
+				"January",
+				"February",
+				"March",
+				"April",
+				"May",
+				"June",
+				"July",
+				"August",
+				"September",
+				"October",
+				"November",
+				"December"
+			  ];
+
+
+
+
+
+
+/*FUNCTIONSSSSSSSSSSSSSSSS */
+
+			function updateCal()
+			{
+				const today = new Date();
+				month.innerText = months[today.getMonth()]; 
+				day.innerText = days[today.getDay()];
+				date.innerText= today.getDate();
+				year.innerText= today.getFullYear();
+				time.innerHTML = today.getHours() +":" +today.getMinutes() +":" +today.getSeconds();
+			}
+
+			setInterval(updateCal, 1000);
+
+			function updateName()
+			{
+				const random = Math.floor(Math.random() * calendarInIndianLanguages.length);
+				name.innerHTML=	calendarInIndianLanguages[random];
+				
+			}
+			setInterval(updateName,2000);
+
+
+			
+			
+
+
+			  
